0339f0de39fe18ced7a33b9b266ffc9d859c092b
Supun Chamuditha <3supun@gmail.com>
2024-12-16 22:23:03 +0530

    Finalize skip login

--- lib/screens/my_appointments_screen.dart
+++ lib/screens/my_appointments_screen.dart
@@ -109,12 +109,7 @@ class _MyAppointmentsScreenState extends State<MyAppointmentsScreen> {
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+        
         title: const Text(
           "My Appointments",
           style: TextStyle(color: Colors.black),
