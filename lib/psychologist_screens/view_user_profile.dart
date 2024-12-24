67ea29a2cafe8be1fcc9e4efe54d7d926a2a7612
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-12-24 12:54:26 +0530

    Refactor reminder card to include patient and appointment IDs

--- lib/psychologist_screens/view_user_profile.dart
+++ lib/psychologist_screens/view_user_profile.dart
@@ -1,93 +1,106 @@
 import 'package:flutter/material.dart';
 import 'package:nuranest/screens/chat_screen.dart';
 
-class ViewUserProfileScreen extends StatelessWidget {
+class ViewUserProfileScreen extends StatefulWidget {
+  final Map<String, dynamic> user;
+
+  const ViewUserProfileScreen({Key? key, required this.user}) : super(key: key);
+
+  @override
+  _ViewUserProfileScreenState createState() => _ViewUserProfileScreenState();
+}
+
+class _ViewUserProfileScreenState extends State<ViewUserProfileScreen> {
+  @override
+  void initState() {
+    super.initState();
+    debugPrint('User: ${widget.user}');
+  }
+
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(
-      backgroundColor: Colors.white,
-      elevation: 0,
-      title: const Text(
-        'Supun Maduranga',
-        style: TextStyle(color: Colors.black),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          'Supun Maduranga',
+          style: TextStyle(color: Colors.black),
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
       ),
-      leading: IconButton(
-        icon: const Icon(Icons.arrow_back, color: Colors.black),
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        
-      ),
-      
-    ),
-    
-    body: SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Enlarged Profile Image
-          const SizedBox(height: 10),
-          Container(
-            padding: const EdgeInsets.all(4), // Border thickness
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(color: Colors.black, width: 4), // Black border
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Enlarged Profile Image
+            const SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.all(4), // Border thickness
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border:
+                    Border.all(color: Colors.black, width: 4), // Black border
+              ),
+              child: CircleAvatar(
+                radius: 100, // Size of the image
+                backgroundColor: Colors.grey.shade300,
+                backgroundImage: AssetImage("lib/assets/images/18.png"),
+              ),
             ),
-            child: CircleAvatar(
-              radius: 100, // Size of the image
-              backgroundColor: Colors.grey.shade300,
-              backgroundImage: AssetImage("lib/assets/images/18.png"),
+
+            const SizedBox(height: 16),
+
+            // Contact options moved below image
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Image.asset('lib/assets/icon/phone.png'),
+                  onPressed: () {},
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                ),
+                IconButton(
+                  icon: Image.asset('lib/assets/icon/email.png'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ChatScreen('Chat Partner Name'),
+                      ),
+                    );
+                  },
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                ),
+                IconButton(
+                  icon: Image.asset('lib/assets/icon/video.png'),
+                  onPressed: () {},
+                  padding: const EdgeInsets.only(left: 20, right: 0),
+                ),
+              ],
             ),
-          ),
 
+            const SizedBox(height: 16),
 
+            // Name and title
+            const Text(
+              'Supun Maduranga',
+              style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold), // Slightly larger font
+            ),
+            const Text(
+              '21 years old',
+              style: TextStyle(fontSize: 17, color: Colors.grey),
+            ),
+            const SizedBox(height: 16),
 
-          const SizedBox(height: 16),
-
-          // Contact options moved below image
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: Image.asset('lib/assets/icon/phone.png'),
-                onPressed: () {},
-                padding: const EdgeInsets.only(left: 20 , right: 20),
-              ),
-              IconButton(
-                icon: Image.asset('lib/assets/icon/email.png'),
-                onPressed: () {
-                  Navigator.push(
-                  context,
-                    MaterialPageRoute(
-                    builder: (context) => ChatScreen('Chat Partner Name'),
-                  ),
-                  );
-                },
-                padding: const EdgeInsets.only(left: 20 , right: 20),
-              ),
-              IconButton(
-                icon: Image.asset('lib/assets/icon/video.png'),
-                onPressed: () {},
-                padding: const EdgeInsets.only(left: 20 , right: 0),
-              ),
-            ],
-          ),
-
-          const SizedBox(height: 16),
-
-          // Name and title
-          const Text(
-            'Supun Maduranga',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold), // Slightly larger font
-          ),
-          const Text(
-            '21 years old',
-            style: TextStyle(fontSize: 17, color: Colors.grey),
-          ),
-          const SizedBox(height: 16),
-        
             const SizedBox(height: 20),
 
             // New reminder card
@@ -104,7 +117,6 @@ class ViewUserProfileScreen extends StatelessWidget {
             ),
             const SizedBox(height: 8),
 
-
             const Align(
               alignment: Alignment.centerLeft,
               child: Text(
@@ -117,10 +129,10 @@ class ViewUserProfileScreen extends StatelessWidget {
               alignment: Alignment.centerLeft,
               child: Text(
                 "No 18/75 ,address hello road colombo 08",
-              style: TextStyle(fontSize: 14, color: Colors.black54),
+                style: TextStyle(fontSize: 14, color: Colors.black54),
               ),
             ),
-            
+
             const SizedBox(height: 8),
             const Align(
               alignment: Alignment.centerLeft,
@@ -133,8 +145,8 @@ class ViewUserProfileScreen extends StatelessWidget {
             const Align(
               alignment: Alignment.centerLeft,
               child: Text(
-              "Message hello , boot ekak kaala inne , ane mnda , mona karannada terenne naa, udwwak karanna doctor",
-              style: TextStyle(fontSize: 14, color: Colors.black54),
+                "Message hello , boot ekak kaala inne , ane mnda , mona karannada terenne naa, udwwak karanna doctor",
+                style: TextStyle(fontSize: 14, color: Colors.black54),
               ),
             ),
             const SizedBox(height: 8),
@@ -150,8 +162,8 @@ class ViewUserProfileScreen extends StatelessWidget {
             const Align(
               alignment: Alignment.centerLeft,
               child: Text(
-              "1 hour",
-              style: TextStyle(fontSize: 14, color: Colors.black54),
+                "1 hour",
+                style: TextStyle(fontSize: 14, color: Colors.black54),
               ),
             ),
             const SizedBox(height: 8),
@@ -167,12 +179,11 @@ class ViewUserProfileScreen extends StatelessWidget {
             const Align(
               alignment: Alignment.centerLeft,
               child: Text(
-              "Online",
-              style: TextStyle(fontSize: 14, color: Colors.black54),
+                "Online",
+                style: TextStyle(fontSize: 14, color: Colors.black54),
               ),
             ),
             const SizedBox(height: 8),
-
           ],
         ),
       ),
@@ -180,8 +191,7 @@ class ViewUserProfileScreen extends StatelessWidget {
   }
 }
 
-
- Widget _buildReminderCard(BuildContext context) {
+Widget _buildReminderCard(BuildContext context) {
   return Center(
     child: Container(
       padding: const EdgeInsets.only(left: 0, right: 0, top: 16, bottom: 0),
@@ -205,16 +215,19 @@ class ViewUserProfileScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Padding(
-                padding: const EdgeInsets.only(left: 10.0), // Left padding for the image
+                padding: const EdgeInsets.only(
+                    left: 10.0), // Left padding for the image
                 child: CircleAvatar(
                   radius: 24,
-                  backgroundImage: const AssetImage("lib/assets/images/reminder_avatar.png"), // Replace with your actual path
+                  backgroundImage: const AssetImage(
+                      "lib/assets/images/reminder_avatar.png"), // Replace with your actual path
                 ),
               ),
               const SizedBox(width: 12),
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.only(left: 10.0), // Left padding for the reminder text
+                  padding: const EdgeInsets.only(
+                      left: 10.0), // Left padding for the reminder text
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: const [
@@ -273,15 +286,17 @@ class ViewUserProfileScreen extends StatelessWidget {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) =>  ViewUserProfileScreen()),
-                    );
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(
+                    //       builder: (context) => ViewUserProfileScreen()),
+                    // );
                   },
                   style: ElevatedButton.styleFrom(
                     padding: const EdgeInsets.all(0),
                     shape: const CircleBorder(),
-                    backgroundColor: const Color(0xFFFFE86C), // Yellowish button color
+                    backgroundColor:
+                        const Color(0xFFFFE86C), // Yellowish button color
                   ),
                   child: const Icon(
                     Icons.play_arrow,
